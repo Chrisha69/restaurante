475741b9b36a855d6fe0ff9f04196fce3e2533fe
Chris <Chris@DESKTOP-70TRLKD>
2023-06-22 10:48:18 -0500

    excepcion

---
+++ restaurante/src/main/java/restaurant/backend/exception/ApiException.java
@@ -0,0 +1,15 @@
+package restaurant.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value =  HttpStatus.NOT_FOUND)
+public class ApiException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ApiException(String mensaje) {
+		super(mensaje);
+	}
+
+}
